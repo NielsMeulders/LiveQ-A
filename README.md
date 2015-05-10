0b9e7c38dec9d74d114b3e39c8949a1db346f8ec
NielsMeulders <niels.meulders@telenet.be>
2015-05-10 23:22:24 +0200

    added some style

--- README.md
+++ README.md
@@ -1,20 +1,12 @@
-Live Q&A app
-============
-By Niels
---------------------
-###Doelgroep
-* IMD-studenten
-* Bezoekers die naar een presentatie komen in de Creativity Gym
-* Bezoekers van een conferentie
+# Node.js Tutorial Socket
 
-###Doel
-* De app zal worden gebruikt om vragen te stellen aan de doelgroep en te beantwoorden door de doelgroep
-* Tijdens presentaties en lessen kan er een discussie gevoerd worden over het desbetreffende onderwerp
+## Install
 
-###De gebruiker
-* Kan zich registereen/inloggen via passport.js
-* Kan een nieuwe discussie starten en de link hiervan delen met anderen
-* Hou de geolocatie bij zodat anderen documenten "in de buurt" kunnen bekijken
-* Kan vragen stellen
-* Kan antwoorden geven op vragen zoals een comment op facebook
-* Links naar afbeeldingen worden automatisch getoond
+
+```sh
+$ mongod
+$ npm install
+$ node app.js
+```
+
+App is now running on **http://localhost:3003**.
--- app.js
+++ app.js
@@ -1,60 +1,78 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+	app = express(),
+	http = require('http').Server(app),
+	mongoose = require('mongoose'),
+	swig = require('swig'),
+	bodyParser = require('body-parser'),
+	path = require('path'),
+	restful = require('node-restful'),
+	io = require('socket.io')(http);
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+mongoose.connect('mongodb://localhost/imd');
 
-var app = express();
-
-// view engine setup
+app.engine('html', swig.renderFile);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes.index);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/', function(req,res) {
+res.render('index');
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.get("/detail.html", function(req, res) {
+    res.render('detail');
 });
 
+var Product = app.product = restful.model('Product', mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	amount: {
+		type: Number,
+		required: true,
+		default: 0
+	}
+}, {collection: 'product'})).methods(['get', 'post', 'put', 'delete']);
+Product.register(app, '/product');
+
+var Message = app.message = restful.model('Message', mongoose.Schema({
+	message: {
+		type: String,
+		required: true
+	},
+    author: {
+        name: String,
+        required: false
+    }
+}, {collection: 'message'})).methods(['get', 'post', 'put', 'delete']);
+Message.register(app, '/message');
+
+io.on('connection', function(socket) {
+	console.log('user connected');
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
+	socket.on('message', function(a) {
+		Message.create({message: a}, function(err, b) {
+			console.log('b', b);
+			io.emit('update', b);
+		});
+	});
+});
+io.on('connect', function(socket) {
+	Message.find()
+		.exec(function(err, messages) {
+			socket.emit('startMessages', messages);
+		});
+});
+
+
+var server = http.listen(3003, function(){
+	console.log('Server running on http://localhost:3003');
+});
 
 module.exports = app;
